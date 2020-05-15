61de37042d7009ec6251f3bfc2d5bd723d2126e5
SangIk-Jo <npngj95@naver.com>
2020-05-15 19:41:13 +0900

    WebBoard_(adminMain, userManagement)_JSI

---
+++ src/main/webapp/resources/js/userManagement.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	
+	var flag = true;
+	$("#checkAll").click(function() {
+		var checkLength = $("input:checkbox[name=userCheck]").legnth;
+
+			if(flag) {
+				$("input:checkbox[name=userCheck]").prop("checked", true);
+				flag = false;
+			} else {
+				$("input:checkbox[name=userCheck]").prop("checked", false);
+				flag = true;
+			}
+			
+	});
+	
+	$("#deleteUser").click(function() {
+		document.userManagement.submit();
+	});
+	
+	
+});
